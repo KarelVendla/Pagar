2d81b843be25e45b508912f72791ce73db6c0b62
Karel Vendla <vendlakarel@gmail.com>
2019-06-04 09:14:14 +0300

    Create model Pagari

---
+++ Pagar/Pagar/Models/Pagari.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pagar.Models
+{
+    public class Pagari
+    {
+        public int Id { get; set; }
+        public int Kogus { get; set; }
+        public Tooted Toode { get; set; }
+        public DateTime Tähtaeg { get; set; }
+        public string Lisa { get; set; }
+        public bool TuleJärgi { get; set; }
+        public string Aadress { get; set; }
+
+        public enum Tooted
+        {
+            Aleksandri_kook = 1,
+            Amaretto_šokolaadiküpsis = 2,
+            Apelsinikeeks = 3,
+            Banaanirull = 4,
+            Ekleer = 5,
+            Brita_kook = 6
+        }
+
+    }
+}
